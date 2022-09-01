eed81bc95da35fa735d9bcf3c08d9679869a573a
BjarteN <freebattie@hotmail.com>
2022-09-01 16:05:31 +0200

    Initial commit

---
+++ src/TestRomanNumber.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestRomanNumber {
+
+    @Test
+    void turn1InToRomanINumber(){
+        assertEquals("I", getRomanValue(1));
+    }
+
+    private String getRomanValue(int i) {
+            return "";
+    }
+
+}
